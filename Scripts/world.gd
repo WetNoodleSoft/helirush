7a3ebff079341c282e89bf76820da6a068251f40
WetNoodleDev <dev@wetnoodlesoft.com>
2025-07-20 12:45:47 -0700

    v 0.0.2
    Animation updates and player controller.

---
+++ Scripts/world.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var chopper = preload("res://Scenes/chopper.tscn")
+@onready var label: Label = $CanvasLayer/Label
+@onready var player
+@onready var world: Node2D = $"."
+
+
+func _ready() -> void:
+	player = chopper.instantiate()
+	add_child(player)
+	player.position = Vector2(640, 650)
+	
+
+func _process(_delta: float) -> void:
+	label.text = (str(round(player.velocity.x)) + " , " + str(round(player.velocity.y)))
+	
